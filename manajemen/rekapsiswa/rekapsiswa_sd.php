abe18a8724a2f312881a820f0af1cf9668eb41eb
aditfreedom <aditfreedom11@gmail.com>
2021-01-29 10:38:35 +0700

    aaa

--- manajemen/rekapsiswa/rekapsiswa_sd.php
+++ manajemen/rekapsiswa/rekapsiswa_sd.php
@@ -26,30 +26,74 @@
 <!-- melihat isi array data user -->
 <!-- <pre><?php //print_r($data_user); ?></pre>  -->
 
-<table class="table table-hover table-striped" id="data-table" >
+<table class="table table-hover table-bordered" id="data-table" >
 	<thead>
 		<tr>
 			<th rowspan="3" class="text-center">Tahun Ajaran</th>			
-			<th colspan="9" class="text-center">Kelas</th>
-			<th>Total</th>
-			<th>Sub Total</th>
-			<th>Opsi</th>
+			<th colspan="18" class="text-center">Kelas</th>
+			<th rowspan="3" class="text-center">Total</th>
+			<th rowspan="3" class="text-center">Sub Total</th>
+			<th rowspan="3" class="text-center">Opsi</th>
 		</tr>
+		
 		<tr>
-		<td colspan="3">I</td>
-		<td colspan="3">II</td>
-		<td colspan="3">III</td>
-		<td colspan="3">IV</td>
-		<td colspan="3">V</td>
-		<td colspan="3">VI</td>
+		<td colspan="3" class="text-center">I</td>
+		<td colspan="3" class="text-center">II</td>
+		<td colspan="3" class="text-center">III</td>
+		<td colspan="3" class="text-center">IV</td>
+		<td colspan="3" class="text-center">V</td>
+		<td colspan="3" class="text-center">VI</td>
 		</tr>
+
 		<tr>
+		<td class="text-center">LK</td>
+		<td class="text-center">PR</td>
+		<td class="text-center">JLH</td>
+		<td class="text-center">LK</td>
+		<td class="text-center">PR</td>
+		<td class="text-center">JLH</td>
+		<td class="text-center">LK</td>
+		<td class="text-center">PR</td>
+		<td class="text-center">JLH</td>
+		<td class="text-center">LK</td>
+		<td class="text-center">PR</td>
+		<td class="text-center">JLH</td>
+		<td class="text-center">LK</td>
+		<td class="text-center">PR</td>
+		<td class="text-center">JLH</td>
+		<td class="text-center">LK</td>
+		<td class="text-center">PR</td>
+		<td class="text-center">JLH</td>
 		</tr>
 	</thead>
 	<tbody>
 		<?php foreach ($data_guru as $key => $value): ?>
 			
 		<tr>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+		<td class="text-center">-</td>
+
+
+
 			<!-- <td><?php echo $key+1; ?></td>
 			<td><?php echo $value['nama_pegawai']; ?></td>
 			<td><?php echo $value['jk']; ?></td>
@@ -57,12 +101,12 @@
 			<td><?php echo $value['jk']; ?></td>
 			<td><?php echo $value['lulus']; ?></td>
 			<td><?php echo $value['status']; ?></td>	
-			
+			 -->
 			
 			<td>
 				<a href="index.php?halaman=edit_kepegawaian&id=<?php echo $value['id'] ?>" class="btn btn-warning">Edit</a>
 				<a href="index.php?halaman=hapus_kepegawaian&id=<?php echo $value['id']; ?>" class="btn btn-danger" onclick="return confirm('hapus data <?php echo $value["nama_guru"]; ?> ?')">Hapus</a>
-			</td> -->
+			</td>
 		</tr>
 		<?php endforeach ?>
 	</tbody>
